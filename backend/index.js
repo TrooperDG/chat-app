55c934fb13ac7a8912510adb1fa37f0b596d82fc
Vishal Pandit <152159518+TrooperDG@users.noreply.github.com>
2025-04-15 13:48:15 +0530

    updated user routes

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,26 @@ import { configDotenv } from "dotenv";
 configDotenv();
 import express from "express";
 import connectDB from "./db/connection_1.db.js";
-import userRouter from "./routes/user.route.js";
-import { errorMiddleware } from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 const PORT = 5000;
 
 app.get("/", (req, res) => {
   res.send(`<h1>Welcome to home </h1>`);
   // res.json(process.env.MONGODB_URl);
 });
+
+//middlewares
+import userRouter from "./routes/user.route.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
+import messageRouter from "./routes/message.route.js";
+
 app.use("/user", userRouter);
+app.use("/message", messageRouter);
 app.use(errorMiddleware);
 
 app.listen(PORT, () => console.log("server running at http://localhost:5000"));
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { login, register } from "../controllers/user.controller.js";
+import {
+  login,
+  register,
+  getProfile,
+  logout,
+  getOtherUsers,
+} from "../controllers/user.controller.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/login", login);
 userRouter.post("/register", register);
+userRouter.get("/profile", isAuthenticated, getProfile);
+userRouter.post("/logout", isAuthenticated, logout);
+userRouter.get("/other-users", isAuthenticated, getOtherUsers);
 
 export default userRouter;
