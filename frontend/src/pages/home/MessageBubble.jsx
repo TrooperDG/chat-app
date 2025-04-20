aed412e7b1df487d69099c32a370e332ae2694a8
Vishal Pandit <152159518+TrooperDG@users.noreply.github.com>
2025-04-20 12:12:48 +0530

    new detailed Message Bubble

--- frontend/src/pages/home/MessageBubble.jsx
+++ frontend/src/pages/home/MessageBubble.jsx
@@ -8,15 +8,36 @@ function MessageBubble({ messageDetails }) {
   return (
     messageDetails &&
     messageDetails.message && (
+      // <div className={`chat ${side === "start" ? "chat-start" : "chat-end"}`}>
+      //   <div
+      //     className={`chat-bubble ${
+      //       side === "start" ? "chat-bubble-neutral" : "chat-bubble-primary"
+      //     }`}
+      //     // className={`chat-bubble chat-bubble-primary`}
+      //   >
+      //     {messageDetails.message}
+      //   </div>
+      // </div>
       <div className={`chat ${side === "start" ? "chat-start" : "chat-end"}`}>
+        <div className={`chat-image avatar`}>
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src={userData?.avatar}
+            />
+          </div>
+        </div>
+        <div className="chat-header">
+          <time className="text-xs opacity-50">12:46</time>
+        </div>
         <div
           className={`chat-bubble ${
             side === "start" ? "chat-bubble-neutral" : "chat-bubble-primary"
           }`}
-          // className={`chat-bubble chat-bubble-primary`}
         >
           {messageDetails.message}
         </div>
+        <div className="chat-footer opacity-50">Seen at 12:46</div>
       </div>
     )
   );
