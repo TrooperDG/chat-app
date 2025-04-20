79d29cdbf9d61e690c40367196e82977fc3044b7
Vishal Pandit <152159518+TrooperDG@users.noreply.github.com>
2025-04-20 11:44:53 +0530

    socket load in home and chat user online

--- frontend/src/pages/home/ChatUser.jsx
+++ frontend/src/pages/home/ChatUser.jsx
@@ -1,19 +1,27 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setSelectedUser } from "../../store/slices/user/user.slice";
 
 function ChatUser({ userData = {} }) {
   const dispatch = useDispatch();
+  const { onlineUsers } = useSelector((state) => state.socketReducer);
+
+  const isOnline = onlineUsers?.includes(userData?._id);
+  // console.log(isOnline, onlineUsers);
+
   const handleSelectUser = () => {
     dispatch(setSelectedUser(userData));
   };
+
   return (
     <li
       onClick={handleSelectUser}
       className=" border-b border-b-gray-300 dark:border-b-gray-700  p-2"
     >
       <div className="flex gap-3  cursor-pointer ">
-        <div className="avatar avatar-online">
+        <div
+          className={`avatar ${isOnline ? "avatar-online" : "avatar-offline"} `}
+        >
           <div className="w-12  rounded-full">
             {/* <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" /> */}
             <img src={userData?.avatar} />
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "./Sidebar";
 import ChatBox from "./ChatBox";
 import { Logout } from "../../components";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  initializeSocket,
+  setOnlineUsers,
+} from "../../store/slices/socket/socket.slice";
+import { addNewMessage } from "../../store/slices/message/message.slice";
 
 function HomePage() {
+  const dispatch = useDispatch();
+  const { userData, isAuthenticated } = useSelector(
+    (state) => state.userReducer
+  );
+  // console.log(userData);
+  const { socket } = useSelector((state) => state.socketReducer);
+
+  useEffect(() => {
+    if (isAuthenticated) dispatch(initializeSocket(userData?._id));
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+    if (!socket) return;
+    socket.on("onlineUsers", (onlineUsers) => {
+      // console.log(onlineUsers);
+      dispatch(setOnlineUsers(onlineUsers));
+    });
+    socket.on("newMessage", (message) => {
+      dispatch(addNewMessage(message));
+    });
+    return () => {
+      socket.close();
+    };
+  }, [socket]);
+
   return (
     <div className="h-screen flex flex-col">
       <div className="flex border-b border-b-gray-300 dark:border-b-gray-700">
