edfc72cc17863891d259028151dfe687796b8512
Vishal Pandit <152159518+TrooperDG@users.noreply.github.com>
2025-04-20 13:56:01 +0530

    userAvatar

--- frontend/src/pages/home/MessageBubble.jsx
+++ frontend/src/pages/home/MessageBubble.jsx
@@ -2,42 +2,41 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function MessageBubble({ messageDetails }) {
-  const { userData } = useSelector((state) => state.userReducer);
-  const side = userData?._id === messageDetails?.senderId ? "end" : "start";
+  const { userData, selectedUserData } = useSelector(
+    (state) => state.userReducer
+  );
+  const isMe = userData?._id === messageDetails?.senderId;
+  const time = new Date(messageDetails.createdAt).toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+
+  // console.log(messageDetails, selectedUserData);
 
   return (
     messageDetails &&
-    messageDetails.message && (
-      // <div className={`chat ${side === "start" ? "chat-start" : "chat-end"}`}>
-      //   <div
-      //     className={`chat-bubble ${
-      //       side === "start" ? "chat-bubble-neutral" : "chat-bubble-primary"
-      //     }`}
-      //     // className={`chat-bubble chat-bubble-primary`}
-      //   >
-      //     {messageDetails.message}
-      //   </div>
-      // </div>
-      <div className={`chat ${side === "start" ? "chat-start" : "chat-end"}`}>
+    messageDetails?.message && (
+      <div className={`chat ${isMe ? "chat-end" : "chat-start"}`}>
         <div className={`chat-image avatar`}>
           <div className="w-10 rounded-full">
             <img
               alt="Tailwind CSS chat bubble component"
-              src={userData?.avatar}
+              src={isMe ? userData?.avatar : selectedUserData.avatar}
             />
           </div>
         </div>
         <div className="chat-header">
-          <time className="text-xs opacity-50">12:46</time>
+          <time className="text-xs opacity-50">{time}</time>
         </div>
         <div
           className={`chat-bubble ${
-            side === "start" ? "chat-bubble-neutral" : "chat-bubble-primary"
+            isMe ? "chat-bubble-primary" : "chat-bubble-neutral"
           }`}
         >
           {messageDetails.message}
         </div>
-        <div className="chat-footer opacity-50">Seen at 12:46</div>
+        {/* <div className="chat-footer opacity-50">Seen at 12:46</div> */}
       </div>
     )
   );
