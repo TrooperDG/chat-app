93e0fefd71582bd0f17edda231c4d62cfb76bce5
Vishal Pandit <152159518+TrooperDG@users.noreply.github.com>
2025-04-29 20:42:54 +0530

    updated sidbarUser style

--- frontend/src/components/UserSidebar/MessageUser.jsx
+++ frontend/src/components/UserSidebar/MessageUser.jsx
@@ -9,29 +9,8 @@ import { IoCheckmarkDone } from "react-icons/io5";
 function MessageUser({ userData = {} }) {
   const dispatch = useDispatch();
   const { onlineUsers } = useSelector((state) => state.socketReducer);
-  // const { notifications, latesMessages } = useSelector(
-  //   (state) => state.messageReducer
-  // );
-
-  // const userNotifications = notifications.filter(
-  //   (message) => message.senderId === userData._id
-  // );
-  // const userLatestMessage = latesMessages.find(
-  //   (message) =>
-  //     message.senderId === userData._id || message.receiverId === userData._id
-  // );
-  // console.log("--------", userNotifications);
-  // const latestMessage =
-  //   userNotifications && userNotifications?.length > 0
-  //     ? userNotifications.reduce((latest, message) =>
-  //         new Date(message.createdAt) > new Date(latest.createdAt)
-  //           ? message
-  //           : latest
-  //       )
-  //     : null;
 
   const isOnline = onlineUsers?.includes(userData?._id);
-  // console.log(isOnline, onlineUsers);
 
   const handleSelectUser = () => {
     dispatch(setSelectedUser(userData));
@@ -50,7 +29,7 @@ function MessageUser({ userData = {} }) {
             <img src={userData?.avatar} />
           </div>
         </div>
-        <div className="overflow-x-hidden">
+        <div className="overflow-x-hidden ">
           <h3>{userData?.username}</h3>
           <p className="text-sm opacity-70 flex items-center gap-0.5">
             {userData?.latestMessage?.receiverId === userData._id && (
@@ -63,7 +42,7 @@ function MessageUser({ userData = {} }) {
                 )}
               </span>
             )}
-            <span>
+            <span className="truncate">
               {userData?.latestMessage?.message || (
                 <small className="text-gray-400 ">Start a chat</small>
               )}
