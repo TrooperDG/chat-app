1c51aa5551eee90219f788c14b42c7793e3b1ce6
Vishal Pandit <152159518+TrooperDG@users.noreply.github.com>
2025-05-19 19:24:56 +0530

    removed unnecessary console logs

--- frontend/src/components/utilities/feedbackSounds.js
+++ frontend/src/components/utilities/feedbackSounds.js
@@ -2,25 +2,21 @@ const playSeenSound = () => {
   const seenSound = new Audio("/sounds/message-seen.mp3");
   seenSound.volume = 0.3;
   seenSound.play();
-  console.log(":seen");
 };
 const playNotificationSound = () => {
   const notificationSound = new Audio("/sounds/droplet.mp3");
   notificationSound.volume = 0.3;
   notificationSound.play();
-  console.log(":noti");
 };
 const playSendSound = () => {
   const sendSound = new Audio("/sounds/message-send.wav");
   sendSound.volume = 0.3;
   sendSound.play();
-  console.log(":send");
 };
 const playReceiveSound = () => {
   const sendSound = new Audio("/sounds/message-received.mp3");
   sendSound.volume = 0.2;
   sendSound.play();
-  console.log(":receive");
 };
 
 export {
